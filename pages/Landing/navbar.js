9d66fdcda0367eeb8ff808918a3b460bcaec98ab
Kanishk Chaudhary <kansihk.chaudhary@yahoo.com>
2022-11-06 09:48:02 -0500

    loginsignup

---
+++ pages/Landing/navbar.js
@@ -0,0 +1,32 @@
+document.write(`
+<html>
+<head>
+    <link rel="stylesheet" href="loginNavbar.css" />
+</head>
+<body>
+    <nav class = "navbar">
+        <div class="logo"></div>
+      
+            <ul class="nav-links">
+                
+            <div class="menu">
+                <li>
+                    <a href="/pages/Landing/login.html">
+                        <div style="background-color: transparent;" id ="navButton">
+                            Log In
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="/pages/Landing/signup.html">
+                        <div style="background-color: white; color: black; padding:8 45" id ="navButton">
+                            Sign Up
+                        </div>
+                    </a>
+                </li>
+            </div>
+        </ul>
+    </nav>
+</body>
+</html>
+`)
